47a07d6b0ea193ace63632293fea850920413434
Adam Chyła <adam@chyla.org>
2014-12-29 22:01:36 +0100

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2014 Adam Chyła, adam@chyla.org
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+
+
+int
+main(int argc, char *argv[])
+{
+  return 0;
+}
